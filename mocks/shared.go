f1634503f0940573d5e7bc17b7e59abd61219c84
Stephen Levine <stephen.levine@gmail.com>
2017-10-08 22:39:11 -0400

    Remove bulk of cflocal-specific code

--- mocks/shared.go
+++
@@ -1,8 +0,0 @@
-package mocks
-
-import "code.cloudfoundry.org/cli/plugin"
-
-//go:generate mockgen -package mocks -destination cli_connection.go github.com/sclevine/cflocal/mocks CliConnection
-type CliConnection interface {
-	plugin.CliConnection
-}
